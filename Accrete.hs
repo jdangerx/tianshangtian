e29a8b68038e6bc62ff8bbd45390d8a4561bc569
John Xia <john.danger.xia@gmail.com>
2015-10-05 17:51:07 -0400

    haha the recursion wasn't being called in all cases

--- Accrete.hs
+++ Accrete.hs
@@ -22,7 +22,13 @@ combine new@(T.Node LR [l, r]) old@(T.Node _ [a, b]) =
                        then new else old
    T.Node LR [l', r'] | l == r' -> T.Node LR [old, r]
                       | r == l' -> T.Node LR [l, old]
-                      | otherwise -> old
+                      | otherwise ->
+                        let newA = combine new a
+                            newB = combine new b
+                        in
+                         if newA /= a then old {T.subForest = [newA, b]}
+                         else if newB /= b then old {T.subForest = [a, newB]}
+                              else old
    T.Node UD [u', d'] | l == u' || l == d' -> T.Node LR [old, r]
                       | r == u' || r == d' -> T.Node LR [l, old]
                       | otherwise ->
@@ -38,7 +44,13 @@ combine new@(T.Node UD [u, d]) old@(T.Node _ [a, b]) =
                        then new else old
    T.Node LR [l', r'] | u == l' || u == r' -> T.Node UD [old, d]
                       | d == l' || d == r' -> T.Node UD [u, old]
-                      | otherwise -> old
+                      | otherwise ->
+                        let newA = combine new a
+                            newB = combine new b
+                        in
+                         if newA /= a then old {T.subForest = [newA, b]}
+                         else if newB /= b then old {T.subForest = [a, newB]}
+                              else old
    T.Node UD [u', d'] | u == d' -> T.Node UD [old, d]
                       | d == u' -> T.Node UD [u, old]
                       | otherwise ->
@@ -66,7 +78,7 @@ expand _ (D {compKindOf = cmp, fstOf = c, sndOf = c'}) =
     Tri -> Just . T.Node UD $ [cLeaf, T.Node LR [cLeaf, cLeaf]]
     Quad -> Just . T.Node UD $
            [T.Node LR [cLeaf, cLeaf], T.Node LR [cLeaf, cLeaf]]
-    TriVert -> Just . T.Node UD $ [cLeaf, T.Node UD [T.Node (P '冖') [], c'Leaf]]
+    TriVert -> Just . T.Node UD $ [T.Node UD [cLeaf, T.Node (P '冖') []], c'Leaf]
     _ -> Nothing
 
 expand' :: Decomp -> Maybe SuperChar
@@ -79,12 +91,19 @@ main :: IO ()
 main = do
   decomps <- (simplifyDecomps <$>) <$> parseWikimedia
   let seedtree = fromJust . join $ (M.lookup '孔' <$> decomps)
-  let tree'' = combine (fromJust . join $ (M.lookup '孟' <$> decomps)) seedtree
-  let tree' = combine (fromJust . join $ (M.lookup '好' <$> decomps)) tree''
-  -- let tree' = combine (fromJust . join $ (M.lookup '委' <$> decomps)) tree''
+  let tree''' = combine (fromJust . join $ (M.lookup '孟' <$> decomps)) seedtree
+  let tree'' = combine (fromJust . join $ (M.lookup '好' <$> decomps)) tree'''
+  let tree' = combine (fromJust . join $ (M.lookup '学' <$> decomps)) tree''
+  -- let tree' = fromJust . join $ (M.lookup '学' <$> decomps)
+  -- let tree = combine (fromJust . join $ (M.lookup '和' <$> decomps)) tree'
   -- let tree = combine (fromJust . join $ (M.lookup '妝' <$> decomps)) tree'
   let strTree = (\c -> case c of
                        P c' -> [c']
                        LR -> "LR"
+                       UD -> "UD") <$> tree''
+  let strTree' = (\c -> case c of
+                       P c' -> [c']
+                       LR -> "LR"
                        UD -> "UD") <$> tree'
   putStrLn (T.drawTree strTree)
+  putStrLn (T.drawTree strTree')
