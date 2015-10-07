4b2226c3d39a3385e73907779bd51e9277b2242b
John Xia <john.danger.xia@gmail.com>
2015-10-07 18:06:10 -0400

    styles exist...

--- static/client.js
+++ static/client.js
@@ -35,7 +35,7 @@ function handleSuccess(data) {
   }
 }
 
-function render(tree, eltId, topLeft) {
+function render(tree, eltId) {
   var head = tree[0],
       subtree = tree[1],
       elt,
@@ -44,52 +44,51 @@ function render(tree, eltId, topLeft) {
       charDiv,
       subEltA,
       subEltB;
-  topLeft = topLeft || [0, 0];
-  charDiv = document.getElementById("char");
-  charDiv.style.transform = "translate(5em, 5em)";
+
   elt = document.getElementById(eltId);
   if (head === "UD" || head === "LR") {
+    var eltType = head === "UD" ? "div" : "span";
     fst = subtree[0];
     snd = subtree[1];
-    subEltA = document.createElement("span");
+    subEltA = document.createElement(eltType);
     subEltA.id = eltId + head[0];
-    setStyle(subEltA);
-    subEltB = document.createElement("span");
+    subEltB = document.createElement(eltType);
     subEltB.id = eltId + head[1];
-    setStyle(subEltB);
-    charDiv.appendChild(subEltA);
-    charDiv.appendChild(subEltB);
+    elt.appendChild(subEltA);
+    elt.appendChild(subEltB);
     render(fst, subEltA.id);
     render(snd, subEltB.id);
   } else {
-    elt.innerHTML = head;
+    elt.innerHTML = "<span class='c'>" + head + "</span>";
   }
 }
 
-function setStyle(elt) {
-  var scale = [1,1],
-      translate = [0,0],
-      pos,
-      i,
-      p;
-  elt.style.position = "absolute";
-  elt.style.fontSize = SIZE + "px";
-  pos = elt.id.slice(4);
-  for (i = 0; i < pos.length; i++) {
-    p = pos[i];
-    if (p === "L") {
-      scale[0] *= 0.5;
-    } else if (p == "R") {
-      scale[0] *= 0.5;
-      translate[0] += (SIZE - translate[0])/2;
-    } else if (p == "U") {
-      scale[1] *= 0.5;
-    } else if (p == "D") {
-      scale[1] *= 0.5;
-      translate[1] += (SIZE - translate[0])/2;
-    }
-  }
-  elt.style.transform = "scale(" + scale[0] + ", " + scale[1] + ")";
-  elt.style.left = translate[0] + "px";
-  elt.style.top = translate[1] + "px";
-}
+
+// function setStyle(elt) {
+//   var scale = [1,1],
+//       translate = [0,0],
+//       pos,
+//       i,
+//       p;
+//   elt.style.position = "absolute";
+//   elt.style.fontSize = SIZE + "px";
+//   pos = elt.id.slice(4);
+//   for (i = 0; i < pos.length; i++) {
+//     p = pos[i];
+//     if (p === "L") {
+//       scale[0] *= 0.5;
+//     } else if (p == "R") {
+//       scale[0] *= 0.5;
+//       translate[0] += (SIZE - translate[0])/2;
+//       translate[0] -= (SIZE * (1-scale[0]))/2;
+//     } else if (p == "U") {
+//       scale[1] *= 0.5;
+//     } else if (p == "D") {
+//       scale[1] *= 0.5;
+//       translate[1] += (SIZE - translate[0])/2;
+//     }
+//   }
+//   elt.style.transform = "scale(" + scale[0] + ", " + scale[1] + ")";
+//   elt.style.left = translate[0] + "px";
+//   elt.style.top = translate[1] + "px";
+// }
