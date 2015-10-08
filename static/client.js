bb651cdd165923c56f3a4ef4a3a0fdd1e8b5d9e2
John Xia <john.danger.xia@gmail.com>
2015-10-08 14:34:40 -0400

    chars that start with UD work!

--- static/client.js
+++ static/client.js
@@ -27,6 +27,7 @@ function handleSuccess(data) {
   if (data == "no such char found") {
     console.log('no char found you stink');
   } else {
+    console.log(data);
     var parsed = JSON.parse(data),
         list = parsed.slice(-1)[0],
         tree = parsed.slice(0,-1)[0];
@@ -35,72 +36,94 @@ function handleSuccess(data) {
             [
               ["折", []],
               ["LR", [
-                ["口", []],
-                ["合", []]
+                ["LR", [["扌", []],
+                        ["口", []]]],
+                ["UD", [["土", []],
+                        ["土", []]]]
               ]
               ]
             ]
            ];
+
     document.getElementById("list").innerHTML = list;
-    document.getElementById("char").innerHTML = "";
-    render(tree, "char");
+    var charDiv = document.getElementById("char");
+    charDiv.innerHTML = "";
+    render(tree, charDiv);
+    rescale(charDiv);
   }
 }
 
-function render(tree, eltId) {
+function render(tree, elt) {
   var head = tree[0],
       subtree = tree[1],
-      elt,
-      fst,
-      snd,
-      charDiv,
-      subEltA,
-      subEltB;
+      cell;
 
-  elt = document.getElementById(eltId);
-  if (head === "UD" || head === "LR") {
-    var eltType = head === "UD" ? "div" : "span";
-    fst = subtree[0];
-    snd = subtree[1];
-    subEltA = document.createElement(eltType);
-    subEltA.id = eltId + head[0];
-    subEltB = document.createElement(eltType);
-    subEltB.id = eltId + head[1];
-    elt.appendChild(subEltA);
-    elt.appendChild(subEltB);
-    render(fst, subEltA.id);
-    render(snd, subEltB.id);
+  if (head === "UD") {
+    var upTree = subtree[0];
+    var downTree = subtree[1];
+    cell = document.createElement("span");
+    cell.setAttribute("class", "cell");
+
+    var upRow = document.createElement("div");
+    upRow.setAttribute("class", "row");
+
+    var downRow = document.createElement("div");
+    downRow.setAttribute("class", "row");
+
+    cell.appendChild(upRow);
+    cell.appendChild(downRow);
+    elt.appendChild(cell);
+    render(upTree, upRow);
+    render(downTree, downRow);
+  } else if (head === "LR") {
+
+    var leftTree = subtree[0];
+    var rightTree = subtree[1];
+
+    render(leftTree, elt);
+    render(rightTree, elt);
+
   } else {
-    elt.innerHTML = "<span class='c'>" + head + "</span>";
+    cell = document.createElement("span");
+    cell.setAttribute("class", "cell");
+    cell.innerHTML = "<span class='character'>" + head + "</span>";
+    elt.appendChild(cell);
   }
 }
 
 
-// function setStyle(elt) {
-//   var scale = [1,1],
-//       translate = [0,0],
-//       pos,
-//       i,
-//       p;
-//   elt.style.position = "absolute";
-//   elt.style.fontSize = SIZE + "px";
-//   pos = elt.id.slice(4);
-//   for (i = 0; i < pos.length; i++) {
-//     p = pos[i];
-//     if (p === "L") {
-//       scale[0] *= 0.5;
-//     } else if (p == "R") {
-//       scale[0] *= 0.5;
-//       translate[0] += (SIZE - translate[0])/2;
-//       translate[0] -= (SIZE * (1-scale[0]))/2;
-//     } else if (p == "U") {
-//       scale[1] *= 0.5;
-//     } else if (p == "D") {
-//       scale[1] *= 0.5;
-//       translate[1] += (SIZE - translate[0])/2;
-//     }
-//   }
-//   elt.style.transform = "scale(" + scale[0] + ", " + scale[1] + ")";
-//   elt.style.left = translate[0] + "px";
-//   elt.style.top = translate[1] + "px";
-// }
+function rescale(elt) {
+  var children = elt.childNodes;
+  var numChildren = children.length;
+  var chars;
+  for (var i = 0; i < numChildren; i++) {
+    var child = children[i];
+    if (child.className === "row") {
+      child.style.fontSize = 1/numChildren + "em";
+      chars = child.getElementsByClassName("character");
+      for (var j = 0; j < chars.length; j++) {
+        // chars[j].style.transform = "scaleX(" + numChildren + ")";
+        // chars[j].style.transform = "scaleX(" + numChildren + ")";
+        compoundScale(chars[j], numChildren);
+      }
+    } else if (child.className == "cell") {
+      chars = child.getElementsByClassName("character");
+      for (var j = 0; j < chars.length; j++) {
+        // console.log(chars[j].style.transform);
+        compoundScale(chars[j], 1/numChildren);
+        // chars[j].style.transform = "scaleX(" + 1/numChildren + ")";
+      }
+    }
+    rescale(child);
+  }
+}
+
+function compoundScale(elt, scale) {
+  var oldT = elt.getAttribute("style") || "transform: scaleX(1);",
+      oldS = oldT.slice(18, -2),
+      newS = oldS * scale;
+  console.log(oldS, newS);
+  elt.style.transform = "scaleX(" + newS + ")";
+
+  // console.log(oldT, transform);
+}
